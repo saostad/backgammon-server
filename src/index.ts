056feb8a19cd6405e257cc74379d29c6fe088b96
Saeid Ostad <sostad@kajimausa.com>
2021-06-10 19:32:16 -0400

    init commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { config as loadEnvVars } from "dotenv";
+import { writeLog } from "fast-node-logger";
+import type { NodeMode } from "./typings/node/mode";
+import { createLoggerInstance, getCredential } from "./helpers/util";
+
+/* place holder for execution time measuring **/
+const hrstart = process.hrtime();
+/** load process.env variables from .env file */
+loadEnvVars();
+
+/** server mode base on process.env.NODE_ENV */
+let nodeMode: NodeMode = process.env.NODE_ENV || "production";
+
+if (process.env.NODE_ENV) {
+  nodeMode = process.env.NODE_ENV;
+}
+
+/**@step create a logger instance */
+/**@description logger instance to store logs in files located in ./logs directory */
+const logger = await createLoggerInstance(nodeMode);
+
+/**@note put your code below here */
+
+/**
+ * @BEST_PRACTICES how to store credential out of source code
+ * - use operating system credential manager
+ * - use .env file located in project root directory
+ */
+getCredential("test_cred").then(({ account, password }) => {
+  writeLog(`loaded credential: ${account}, ${password}`, {
+    level: "warn",
+    stdout: true,
+  });
+});
+
+process.on("beforeExit", (code) => {
+  const hrend = process.hrtime(hrstart);
+  writeLog(`Execution time ${hrend[0]}s ${hrend[1] / 1000000}ms`, {
+    level: "info",
+    stdout: true,
+  });
+});
